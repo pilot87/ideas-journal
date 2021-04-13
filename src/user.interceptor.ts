d4268369bf08f34b5376bdefe47b931251930317
Alexey A Kostylev aka KernelExplorer <alexkos87@gmail.com>
2021-04-13 21:25:45 +0300

    Auth info implemented fix

--- src/user.interceptor.ts
+++ src/user.interceptor.ts
@@ -8,16 +8,20 @@ import { Observable } from 'rxjs'
 const jwt = require('jsonwebtoken')
 const config = require('config')
 
+// insert user header with true authorise username or empty if guest
 @Injectable()
 export class UserInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const token = context
-      .switchToHttp()
-      .getRequest()
-      .headers.authorization.split(' ')[1]
-    const { username } = jwt.verify(token, config.get('jwtSecret'))
-    context.switchToHttp().getRequest().headers.user = username
-    console.log(context.switchToHttp().getRequest().headers)
+    try {
+      const token = context
+        .switchToHttp()
+        .getRequest()
+        .headers.authorization.split(' ')[1]
+      const { username } = jwt.verify(token, config.get('jwtSecret'))
+      context.switchToHttp().getRequest().headers.user = username
+    } catch (e) {
+      context.switchToHttp().getRequest().headers.user = ''
+    }
     return next.handle()
   }
 }
